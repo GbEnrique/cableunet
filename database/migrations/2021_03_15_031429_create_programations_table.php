9f372089a8eadf81620ab10352aa22a88b49dddf
Endersson Garcia <endersson.garcia.eegb@gmail.com>
2021-03-16 19:25:48 -0400

    version 5

--- database/migrations/2021_03_15_031429_create_programations_table.php
+++ database/migrations/2021_03_15_031429_create_programations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProgramationTable extends Migration
+class CreateProgramationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProgramationTable extends Migration
      */
     public function up()
     {
-        Schema::create('programation', function (Blueprint $table) {
+        Schema::create('programations', function (Blueprint $table) {
              $table->id();
             $table->string('name')->unique();
             $table->time('start_hour')->required();
@@ -32,6 +32,6 @@ class CreateProgramationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('programation');
+        Schema::dropIfExists('programations');
     }
 }
